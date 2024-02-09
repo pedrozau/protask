1b69704570d20eb433f10993777c0798480f682f
pedrozau <pedrozau28@gmail.com>
2024-02-10 00:42:13 +0100

    new chnage

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../views/LoginView.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
